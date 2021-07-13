73ad43f068cd72a4ce83c79e6a814c399b1caaa9
bowdenshot <anthonyelavezzo@gmail.com>
2021-07-13 13:50:59 -0500

    for more than 30 results display link to paginated issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 let issueContainerEl = document.querySelector('#issues-container');
+let limitWarningEl = document.querySelector('#limit-warning');
 let getRepoIssues = function(repo) {
     let apiUrl = 'https://api.github.com/repos/' + repo + '/issues?direction=asc';
     fetch(apiUrl).then(function(response) {
@@ -7,11 +8,28 @@ let getRepoIssues = function(repo) {
         response.json().then(function(data) {
             displayIssues(data);
         });
+
+        // check if api has paginated issues
+        if (response.headers.get('Link')) {
+            displayWarning(repo);
+        }
     }
     else{
         alert('There was a problem with your request!')
     }
 });
+};
+
+let displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = 'To see more than 30 issues visit ';
+    let linkEl = document.createElement('a');
+    linkEl.textContent = 'See More Issues on GitHub.'
+    linkEl.setAttribute('href', 'https://github.com/' + repo + '/issues');
+    linkEl.setAttribute('target', '_blank');
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
 };
 
 let displayIssues = function(issues) {
@@ -47,7 +65,6 @@ let displayIssues = function(issues) {
         
         issueContainerEl.appendChild(issueEl);
     };
-
 };
 
-getRepoIssues('alavezzo/git-it-done');
+getRepoIssues('facebook/react');
